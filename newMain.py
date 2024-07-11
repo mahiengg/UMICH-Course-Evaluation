b8752c28fb0a59f7521b2baa19650886f6e07385
Raja Thiyagarajan <124640440+mahiengg@users.noreply.github.com>
2024-07-11 12:20:28 -0400

    Update newMain.py

--- newMain.py
+++ newMain.py
@@ -26,7 +26,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-os.environ['OPENAI_API_KEY'] = 'sk-'
+os.environ['OPENAI_API_KEY'] = ${{secrets.OPENAI_API}}.
 os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
 
 
